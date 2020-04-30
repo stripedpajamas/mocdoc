c5039267b8f3af7c6ba9492e1a10271a1ee7569e
Peter Squicciarini <stripedpajamas273@gmail.com>
2020-04-30 15:32:04 -0700

    Wrap Dynalite

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
 # mocdoc
+
+Run in-memory DynamoDB instances for parallel testing (e.g. [Ava](https://github.com/avajs/ava)). Heavily inspired by [MongoDB Memory Server](https://github.com/nodkz/mongodb-memory-server); backed by [Dynalite](https://github.com/mhart/dynalite).
+
+## Install
+```shell
+npm install --save-dev mocdoc
+```
+
+## Use
+```javascript
+const AWS = require('aws-sdk')
+const { DynamoDBMemoryServer } = require('mocdoc')
+
+const DynamoDB = new DynamoDBMemoryServer()
+const endpoint = await dynamo.getEndpoint()
+
+const dynamoClient = AWS.DynamoDB({ endpoint })
+```
+
+## Todo
+- [ ] Unit tests
+- [ ] Debug logging
+
+## License
+MIT
--- src/index.js
+++ src/index.js
@@ -1,172 +1,43 @@
-const promiseOrCallbackWithErrorAndData = (error, data, cb) => {
-  if (typeof cb === 'function') {
-    return cb(error, data)
-  }
-  return { promise: () => error ? Promise.reject(error) : Promise.resolve(data) }
-}
+const Dynalite = require('dynalite')
+const getPort = require('get-port')
 
-const parseUpdateExpression = (expr, names, values) => {
-  // Within each clause, there are one or more actions separated by commas.
-  // Each action represents a data modification.
-  const SUPPORTED_ACTIONS = new Set(['SET']) // only supporting SET right now
+const startDynamo = (dynamo, host, port) => new Promise((resolve, reject) => {
+  dynamo.listen(port, host, (err) => {
+    if (err) { return reject(err) }
+    return resolve()
+  })
+})
 
-  const actions = [] // { actionName, path, value }
-  const exprTokens = expr.split(' ')
-  while (exprTokens.length) {
-    const actionName = exprTokens.shift()
-    switch (actionName) {
-      case 'SET': {
-        const pathRaw = exprTokens.shift()
-        const path = typeof names[pathRaw] !== 'undefined' ? names[pathRaw] : pathRaw
-        exprTokens.shift() // =
-        let valueRaw = exprTokens.shift()
-        if (valueRaw[valueRaw.length - 1] === ',') {
-          valueRaw = valueRaw.slice(0, valueRaw.length - 1)
-        }
-        const value = typeof values[valueRaw] !== 'undefined' ? values[valueRaw] : valueRaw
-        actions.push({ actionName, path, value })
-        break
-      }
-      default:
-        break
-    }
-
-    const next = exprTokens[0]
-    if (next && !SUPPORTED_ACTIONS.has(next)) {
-      // if next token isn't a new clause,
-      // reloop pretending to be whatever we were doing previously
-      exprTokens.unshift(actionName)
-    }
-  }
-  return actions
-}
-
-class MockDocumentClient {
-  constructor () {
-    this.tables = new Map() // TableName: String -> { PrimaryKey: String, Data: Map }
+class DynamoDBMemoryServer {
+  constructor (opts = {}) {
+    this.instance = Dynalite({ createTableMs: 15 })
+    this.started = false
+    this.host = opts.host || '127.0.0.1'
+    this.port = opts.port
   }
 
-  load ({ TableName, PrimaryKey, Items }) {
-    if (typeof PrimaryKey !== 'string') {
-      throw new Error('Primary Key must be specified and must be a string')
+  async getEndpoint () {
+    const host = this.host
+    const port = this.port || await getPort()
+
+    this.port = port
+
+    const endpoint = `http://${host}:${port}`
+
+    if (!this.started) {
+      await startDynamo(this.instance, host, port)
+      this.started = true
     }
-    if (this.tables.has(TableName)) {
-      const table = this.tables.get(TableName)
-      if (table.PrimaryKey !== PrimaryKey) {
-        throw new Error(`Table already exists with different primary key: ${table.PrimaryKey}`)
-      }
-    } else {
-      this.tables.set(TableName, { PrimaryKey, Data: new Map() })
-    }
-    Items.forEach((Item) => this.put({ TableName, Item }))
+
+    return endpoint
   }
 
-  put ({ TableName, Item, ...Params }, cb) {
-    if (!this.tables.has(TableName)) {
-      return promiseOrCallbackWithErrorAndData(
-        new Error(`TableName ${TableName} does not exist`), null, cb
-      )
-    }
-    const table = this.tables.get(TableName)
-    if (typeof Item[table.PrimaryKey] === 'undefined') {
-      return promiseOrCallbackWithErrorAndData(
-        new Error('Incorrect key attribute'), null, cb
-      )
-    }
-
-    table.Data.set(Item[table.PrimaryKey], Item)
-
-    return promiseOrCallbackWithErrorAndData(null, null, cb)
-  }
-
-  get ({ TableName, Key }, cb) {
-    if (!this.tables.has(TableName)) {
-      return promiseOrCallbackWithErrorAndData(
-        new Error(`TableName ${TableName} does not exist`), null, cb
-      )
-    }
-    const table = this.tables.get(TableName)
-    if (typeof Key[table.PrimaryKey] === 'undefined') {
-      return promiseOrCallbackWithErrorAndData(
-        new Error('Incorrect key attribute'), null, cb
-      )
-    }
-
-    const Item = table.Data.get(Key[table.PrimaryKey])
-
-    return promiseOrCallbackWithErrorAndData(null, { Item }, cb)
-  }
-
-  delete ({ TableName, Key, ...Params }, cb) {
-    if (!this.tables.has(TableName)) {
-      return promiseOrCallbackWithErrorAndData(
-        new Error(`TableName ${TableName} does not exist`), null, cb
-      )
-    }
-    const table = this.tables.get(TableName)
-    if (typeof Key[table.PrimaryKey] === 'undefined') {
-      return promiseOrCallbackWithErrorAndData(
-        new Error('Incorrect key attribute'), null, cb
-      )
-    }
-    const Item = table.Data.get(Key[table.PrimaryKey])
-    table.Data.delete(Key[table.PrimaryKey])
-
-    if (Params.ReturnValues === 'ALL_OLD') {
-      return promiseOrCallbackWithErrorAndData(null, { Item }, cb)
-    }
-    return promiseOrCallbackWithErrorAndData(null, null, cb)
-  }
-
-  update ({ TableName, Key, UpdateExpression, ...Params }, cb) {
-    if (!this.tables.has(TableName)) {
-      return promiseOrCallbackWithErrorAndData(
-        new Error(`TableName ${TableName} does not exist`), null, cb
-      )
-    }
-    const table = this.tables.get(TableName)
-    const key = Key[table.PrimaryKey]
-    if (typeof key === 'undefined') {
-      return promiseOrCallbackWithErrorAndData(
-        new Error('Incorrect key attribute'), null, cb
-      )
-    }
-
-    const Item = table.Data.get(key)
-    const names = Params.ExpressionAttributeNames || {}
-    const values = Params.ExpressionAttributeValues || {}
-    const updateActions = parseUpdateExpression(UpdateExpression, names, values)
-
-    let updatedItem = Item
-    const oldUpdatedAttributes = {}
-    const newUpdatedAttributes = {}
-    updateActions.forEach(({ actionName, path, value }) => {
-      switch (actionName) {
-        case 'SET': {
-          oldUpdatedAttributes[path] = Item[path]
-          newUpdatedAttributes[path] = value
-          updatedItem = Object.assign({}, updatedItem, { [path]: value })
-          table.Data.set(key, updatedItem)
-          break
-        }
-        default:
-          break
-      }
-    })
-
-    switch (Params.ReturnValues) {
-      case 'ALL_NEW':
-        return promiseOrCallbackWithErrorAndData(null, { Attributes: updatedItem }, cb)
-      case 'ALL_OLD':
-        return promiseOrCallbackWithErrorAndData(null, { Attributes: Item }, cb)
-      case 'UPDATED_NEW':
-        return promiseOrCallbackWithErrorAndData(null, { Attributes: newUpdatedAttributes }, cb)
-      case 'UPDATED_OLD':
-        return promiseOrCallbackWithErrorAndData(null, { Attributes: oldUpdatedAttributes }, cb)
-      default:
-        return promiseOrCallbackWithErrorAndData(null, {}, cb)
+  async close () {
+    if (this.started) {
+      this.instance.close()
+      this.started = false
     }
   }
 }
 
-module.exports = MockDocumentClient
+module.exports = { DynamoDBMemoryServer }
